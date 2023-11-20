81cd900506c6ae57d9076ef0f9c14fcad04073c6
jonathanchw <jonathanchw@gmail.com>
2023-11-20 01:21:09 -0300

    json keys for content

---
+++ keysforcontent.js
@@ -0,0 +1,119 @@
+[
+    {
+        "key": "title",
+        "type": "text",
+        "label": "Title",
+        "placeholder": "Kids toy shop"
+    },
+    {
+        "key": "store_name",
+        "type": "text",
+        "label": "Store name",
+        "placeholder": "Kids toy shop"
+    },
+    {
+        "key": "top_banner_title",
+        "type": "text",
+        "label": "Top banner title",
+        "placeholder": "Explore Our Selection Of High-quality"
+    },
+    {
+        "key": "top_banner_subtitle",
+        "type": "text",
+        "label": "Top banner subtitle",
+        "placeholder": "enjoy our discounts"
+    },
+    {
+        "key": "top_banner_image",
+        "type": "image",
+        "label": "Top banner image"
+    },
+    {
+        "key": "top_banner_description",
+        "type": "textarea",
+        "label": "Top banner description",
+        "placeholder": "Some great description about this website"
+    },
+    {
+        "key": "welcome_banner_title",
+        "type": "text",
+        "label": "Welcome banner title",
+        "placeholder": "Welcome To Our Store"
+    },
+    {
+        "key": "welcome_description",
+        "type": "textarea",
+        "label": "Welcome_description",
+        "placeholder": "Welcome the user in one paragrahp"
+    },
+    {
+        "key": "welcome_banner_image",
+        "type": "image",
+        "label": "Welcome banner image"
+    },
+    {
+        "key": "about_us_title",
+        "type": "text",
+        "label": "About us title",
+        "placeholder": "Welcome to our online store!"
+    },
+    {
+        "key": "about_us_description",
+        "type": "textarea",
+        "label": "About us description",
+        "placeholder": "Who we are and why we are great!"
+    },
+    {
+        "key": "about_us_image",
+        "type": "image",
+        "label": "About us"
+    },
+    {
+        "key": "benefits_key_background_image",
+        "type": "image",
+        "label": "Benefits key background image"
+    },
+    {
+        "key": "benefits_key_title",
+        "type": "text",
+        "label": "Benefits key title",
+        "placeholder": "Benefits key"
+    },
+    {
+        "key": "benefits_key",
+        "type": "children",
+        "label": "Benefits key items",
+        "children": [
+            {
+                "key": "benefits_key_bullet",
+                "type": "text",
+                "label": "Benefits key item",
+                "placeholder": "Captivating Variety"
+            }
+        ]
+    },
+    {
+        "key": "primary_color",
+        "type": "color",
+        "label": "Primary color",
+        "caption": "Buttons, etc"
+    },
+    {
+        "key": "background_color",
+        "type": "color",
+        "label": "Background color",
+        "caption": "Background of html whole document"
+    },
+    {
+        "key": "text_color",
+        "type": "color",
+        "label": "Text color",
+        "caption": "Titles and paragraphs"
+    },
+    {
+        "key": "footer_color",
+        "type": "color",
+        "label": "Footer color",
+        "caption": "Footer color"
+    }
+]
